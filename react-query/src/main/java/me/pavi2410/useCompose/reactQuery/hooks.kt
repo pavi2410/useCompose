b9f9ed27d163fb438c275111b3d9fcc55e81d616
Pavitra Golchha <hello@pavi2410.me>
2022-01-29 13:49:41 +0530

    feat: useMutation

--- react-query/src/main/java/me/pavi2410/useCompose/reactQuery/hooks.kt
+++ react-query/src/main/java/me/pavi2410/useCompose/reactQuery/hooks.kt
@@ -1,16 +1,27 @@
 package me.pavi2410.useCompose.reactQuery
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.produceState
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.*
 
-sealed class QueryResult<out T> {
-    object Loading : QueryResult<Nothing>()
-    data class Error(val message: Throwable) : QueryResult<Nothing>()
-    data class Content<T>(val data: T) : QueryResult<T>()
+sealed interface QueryResult<out T> {
+    object Loading : QueryResult<Nothing>
+    data class Error(val message: Throwable) : QueryResult<Nothing>
+    data class Content<T>(val data: T) : QueryResult<T>
+}
+//
+//sealed interface MutationResult<T> {
+//    abstract fun mutate(): T
+//
+//    data class
+//}
+
+interface Mutation<T> {
+    fun mutate(vararg args: String, callback: (T) -> Unit)
+    fun cancel()
 }
 
 @Composable
@@ -19,6 +30,22 @@ fun <T> useQuery(query: suspend CoroutineScope.() -> T): State<QueryResult<T>> {
         value = withContext(Dispatchers.IO) {
             val res = query()
             QueryResult.Content(res)
+        }
+    }
+}
+
+@SuppressLint("ComposableNaming")
+@Composable
+fun <T> useMutation(query: suspend CoroutineScope.(args: Array<out String>) -> T): Mutation<T> {
+    val coroutineScope = rememberCoroutineScope()
+    return object: Mutation<T> {
+        override fun mutate(vararg args: String, callback: (T) -> Unit) {
+            coroutineScope.launch {
+                callback(query(args))
+            }
+        }
+        override fun cancel() {
+            coroutineScope.cancel()
         }
     }
 }
